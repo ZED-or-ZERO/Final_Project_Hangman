3f9d72f9bd9ed35efd58d1ac5a39c95ec07f321c
Zhanbolat <akylbaizhanbolat@gmai.com>
2024-05-01 16:14:16 +0500

    Update next steps OOP

--- button.py
+++ button.py
@@ -1,8 +1,7 @@
-import pygame as pg 
-import math 
+import pygame as pg
+import math
 
-from setting import * 
-
+from setting import *
 
 class Button:
     def __init__(self, x, y, letter):
@@ -12,13 +11,13 @@ class Button:
         self.radius = RADIUS
         self.visible = True
 
-    def draw(self, win, font):  # Добавляем аргумент font
+    def draw(self, win, font):
         if self.visible:
             pg.draw.circle(win, BLACK, (self.x, self.y), self.radius, 3)
-            text = font.render(self.letter, 1, BLACK)  # Используем font
-        else: 
+            text = font.render(self.letter, 1, BLACK)
+        else:
             pg.draw.circle(win, BLACK, (self.x, self.y), self.radius)
-            text = font.render(self.letter, 1, WHITE)  # Используем font
+            text = font.render(self.letter, 1, WHITE)
 
         win.blit(text, (self.x - text.get_width() / 2, self.y - text.get_height() / 2))
 
@@ -26,21 +25,3 @@ class Button:
         x, y = mouse_pos
         dis = math.sqrt((self.x - x)**2 + (self.y - y)**2)
         return dis < self.radius
-
-
-
-    def play_button_clicked(self, mouse_pos):
-        # 1. Определите координаты и размеры кнопки "Играть"
-        play_button_x = self.x / 10  # X-координата левого верхнего угла кнопки
-        play_button_y = self.y / 10  # Y-координата левого верхнего угла кнопки
-        play_button_width = 50       # Ширина кнопки
-        play_button_height = 10      # Высота кнопки
-
-        m_x, m_y = mouse_pos
-
-        if (play_button_x <= mouse_x <= play_button_x + play_button_width and
-            play_button_y <= mouse_y <= play_button_y + play_button_height):
-            return True
-        else:
-            return False
-
---
+++ game.py
@@ -0,0 +1,156 @@
+import pygame as pg
+import math
+import random
+
+from setting import *
+from button import Button
+from records import Records
+
+class Game:
+    def __init__(self):
+        pg.init()
+        self.win = pg.display.set_mode((WIDTH, HEIGHT))
+        pg.display.set_caption("Hangman Game!")
+        self.clock = pg.time.Clock()
+        self.hangman_status = 0
+        self.images = []
+
+        for i in range(7):
+            image = pg.image.load(f'images/hangman{i}.png')
+            self.images.append(image)
+
+        self.word_list = open("data/Words.txt").read().split()
+        self.word = random.choice(self.word_list)
+        self.guessed = []
+        self.buttons = self.create_buttons()
+        self.fonts = self.setup_fonts()
+        self.game_over = False
+        self.won = False
+        self.records = Records()
+
+    def setup_fonts(self):
+        LETTER_FONT = pg.font.SysFont('comicsans', LETTER_FONT_SIZE)
+        WORD_FONT = pg.font.SysFont('comicsans', WORD_FONT_SIZE)
+        TITLE_FONT = pg.font.SysFont('comicsans', TITLE_FONT_SIZE)
+        return LETTER_FONT, WORD_FONT, TITLE_FONT
+
+    def create_buttons(self):
+        startx = round((WIDTH - (RADIUS * 2 + GAP) * 13) / 2)
+        starty = 400
+        letters = []
+        A = 65
+        for i in range(26):
+            x = startx + GAP * 2 + ((RADIUS * 2 + GAP) * (i % 13))
+            y = starty + ((i // 13) * (GAP + RADIUS * 2))
+            letters.append(Button(x, y, chr(A + i)))
+        return letters
+
+    def draw(self):
+        self.win.fill(WHITE)
+
+        # Title
+        text = self.fonts[2].render("Hangman Game", 1, BLACK)
+        self.win.blit(text, (WIDTH / 2 - text.get_width() / 2, 20))
+
+        # Word
+        display_word = ""
+        for letter in self.word:
+            if letter in self.guessed:
+                display_word += letter + " "
+            else:
+                display_word += "_ "
+        text = self.fonts[1].render(display_word, 1, BLACK)
+        self.win.blit(text, (400, 200))
+
+        # Buttons
+        for button in self.buttons:
+            button.draw(self.win, self.fonts[0]) 
+
+        # Hangman image
+        self.win.blit(self.images[self.hangman_status], (150, 100))
+        pg.display.update()
+
+    def handle_events(self):
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                pg.quit()
+            elif event.type == pg.KEYDOWN and event.key == pg.K_ESCAPE:
+                self.game_over = True  
+            elif event.type == pg.MOUSEBUTTONDOWN:
+                m_x, m_y = pg.mouse.get_pos()
+                for button in self.buttons:
+                    if button.clicked((m_x, m_y)) and button.visible:
+                        button.visible = False
+                        self.guessed.append(button.letter)
+                        if button.letter not in self.word:
+                            self.hangman_status += 1 
+
+    def check_win_loss(self):
+        won = True
+        for letter in self.word:
+            if letter not in self.guessed:
+                won = False
+                break
+
+        if won:
+            self.display_message("You WON!")
+            self.game_over = True
+            self.won = True
+
+        if self.hangman_status == 6:
+            self.display_message(f"You LOST! The word was {self.word}")
+            self.game_over = True
+            self.won = False
+
+    def reset_game(self):
+        self.hangman_status = 0
+        self.word = random.choice(self.word_list)
+        self.guessed = []
+        for button in self.buttons:
+            button.visible = True
+        self.game_over = False
+        self.won = False
+
+    def play(self):
+        while not self.game_over:
+            self.clock.tick(60)
+            self.handle_events() 
+            self.draw()
+            self.check_win_loss()
+
+    def display_message(self, message):
+        pg.time.delay(1000)
+        self.win.fill(WHITE)
+        text = self.fonts[1].render(message, 1, BLACK)
+        self.win.blit(text, (WIDTH/2 - text.get_width()/2, HEIGHT/2 - text.get_height()/2))
+        pg.display.update()
+        pg.time.delay(3000)
+
+    def main_menu(self):
+        play_game = True
+        while play_game:
+            self.win.fill(WHITE)
+
+            title_text = self.fonts[2].render("Hangman Game", 1, BLACK)
+            play_text = self.fonts[1].render("Play", 1, BLACK)
+            scores_text = self.fonts[1].render("High Scores", 1, BLACK)
+
+            title_rect = title_text.get_rect(center=(WIDTH // 2, HEIGHT // 2 - 100))
+            play_rect = play_text.get_rect(center=(WIDTH // 2, HEIGHT // 2))
+            scores_rect = scores_text.get_rect(center=(WIDTH // 2, HEIGHT // 2 + 100))
+
+            self.win.blit(title_text, title_rect)
+            self.win.blit(play_text, play_rect)
+            self.win.blit(scores_text, scores_rect)
+
+            pg.display.update()
+
+            for event in pg.event.get():
+                if event.type == pg.QUIT:
+                    play_game = False
+                elif event.type == pg.MOUSEBUTTONDOWN:
+                    mouse_pos = pg.mouse.get_pos()
+                    if play_rect.collidepoint(mouse_pos):
+                        self.play()
+                    elif scores_rect.collidepoint(mouse_pos):
+                        self.records.show_high_scores(self) 
